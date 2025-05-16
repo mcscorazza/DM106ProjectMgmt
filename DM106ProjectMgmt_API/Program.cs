8439389c63a4c6fdd136673132d6a94359cf16fc
Marcos <mcs.corazza@gmail.com>
2025-05-16 20:19:52 -0300

    Aula 2.4

--- DM106ProjectMgmt_API/Program.cs
+++ DM106ProjectMgmt_API/Program.cs
@@ -1,6 +1,16 @@
+using System.Text.Json.Serialization;
+using DM106ProjectMgmt.Shared.Data.DB;
+using DM106ProjectMgmt.Shared.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    var dal = new DAL<MachineDesign>(new DM106ProjectMgmtContext());
+    return dal.Read();
+}
+);
 
 app.Run();
